d21f3e7a8c6f71bec5520544117f0666cee0e5ed
JohnKougioum <73065650+JohnKougioum@users.noreply.github.com>
2021-04-12 02:12:30 +0300

    navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Header />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+$main-blue: #3a89fa;
+$main-grey: #ebebeb;
+$dark-grey: #a7a4a4;
 </style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <nav>
+      <div class="logo">
+        <!-- <a href="/">Logo</a> -->
+        <img src="../assets/images/Logo.svg" alt="Logo" />
+      </div>
+
+      <div class="nav-search">
+        <form action="">
+          <div class="nav-search-items">
+            <input type="text" name="" id="" placeholder="Αναζήτηση" />
+            <button type="submit">
+              <div class="button-icon">
+                <img src="../assets/images/Search.svg" alt="sadf" />
+              </div>
+            </button>
+          </div>
+        </form>
+      </div>
+      <div class="login">
+        <input type="button" value="Login" />
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+@import "../assets/css/navbar/navbar.css";
+</style>
