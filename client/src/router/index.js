179b1909c03002b3aad8e3769e2aed4d4e5641c6
tolismaloudis <73343451+tolismaloudis@users.noreply.github.com>
2021-04-22 14:21:19 +0300

    fe

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from "../views/Index";
+import Home from "../views/Home";
 import Auth from "../components/Auth";
+import NewPost from "../views/NewPost";
+import ClickedPost from "../views/ClickedPost";
 
 const login_red = `https://login.iee.ihu.gr/authorization/?client_id=${process.env.VUE_APP_CLIENT_ID}&response_type=code
 &scope=profile&redirect_uri=http://localhost:8080/login/auth
@@ -11,6 +14,21 @@ const routes = [
     path: "/",
     name: "Index",
     component: Index,
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: '/newPost',
+    name: "NewPost",
+    component: NewPost
+  },
+  {
+    path: '/clickedPost',
+    name: "ClickedPost",
+    component: ClickedPost
   },
   {
     path: "/login",
